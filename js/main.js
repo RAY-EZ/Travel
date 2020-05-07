075b84443a54a5e62013b41685bfae11b738c8f5
Sushil Kumar <sushilz25558@gmail.com>
2020-05-07 15:45:07 +0530

    My first Git commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+const hamMenu= document.getElementById('menu');
+const toggle = document.querySelector('.toggle.close');
+const navList = document.querySelector('.nav-list');
+
+/*****************************************************************
+NavList Here -- Forgive me For Naive Approach I'm fucking dumb lol
+******************************************************************/
+hamMenu.addEventListener('click',function(e){
+    navList.style.display="block";
+    navList.style.position="fixed";
+    navList.addEventListener('onfocusout',function(e){
+        navList.style.display="none";
+    });
+});
+
+toggle.addEventListener('click',function(e){
+    navList.style.display="none";
+    navList.style.position="absolute";
+});
+window.onresize= function(e){
+    if(window.innerWidth>768){
+        navList.style.display="block";
+        navList.style.position="relative";
+    }else{
+        navList.style.display="none";
+        navList.style.position="absolute";
+    } 
+}
+
+/************************************************
+ Photo Slider or Photo menu Section 3rd
+ ************************************************/
+const img = document.querySelector('.second .right');
+const slider = document.querySelector('.slider');
+var show = document.querySelector('.show');
+//use event delegation for accessing arrow keys 
+//move show class to hightlight Image number 
+//Copy the value of show class into background url
+// Algo to make it work
+slider.addEventListener('click',function(e){
+    if(e.target.classList.contains('fa-arrow-right')){
+        var nextSib = show.nextElementSibling;
+        nextSib.setAttribute('class','show');
+        img.style.backgroundImage='url('+nextSib.firstElementChild.getAttribute('href')+')';
+        show.classList.remove('show');
+        show= nextSib;
+    }
+    if(e.target.classList.contains('fa-arrow-left')){
+        var prevSib = show.previousElementSibling;
+        prevSib.setAttribute('class','show'); img.style.backgroundImage='url('+ prevSib.firstElementChild.getAttribute('href')+ ')';
+        show.classList.remove('show');
+        show= prevSib;
+    }
+});
